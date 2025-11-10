e8553568c2e267de2ad609222242aef5a1f66e93
Foysal-Munsy <bdfoysal555@gmail.com>
2025-11-10 11:30:44 +0600

    feat(admin): add input validation for User Category 1 in CreateUserDto

--- src/admin/dto/create-user.dto.ts
+++ src/admin/dto/create-user.dto.ts
@@ -1,5 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsEmail, IsNotEmpty, IsOptional, Matches } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Name is required' })
+  @Matches(/^[A-Za-z\s]+$/, { message: 'Name contain only alphabet' })
   name: string;
+
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail()
+  @Matches(/\.xyz$/)
   email: string;
   role: string; // host or guest
+
+  @IsNotEmpty({ message: 'NID number is required' })
+  @Matches(/^\d{10}$/, { message: 'NID must be 10 digits' })
+  nidNumber: string;
+
+  @IsOptional()
+  nidImage?: Express.Multer.File;
 }
